5eeb8c5e40618417c0d0a12f8e3cefd092f29727
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2024-10-08 21:14:50 +0530

    feat: add about

---
+++ src/lib/utils.ts
@@ -0,0 +1,9 @@
+interface NavBarItem {
+  name: string;
+  href: string;
+}
+
+export const navBarItems: NavBarItem[] = [
+  { name: "about", href: "/about" },
+//   { name: "contact", href: "/contact" },
+];
